1eab9fcb370b88a0331e6352f2b3b319063ae8d0
Zamzam93 <samira0101@live.com>
2022-07-30 12:00:03 -0500

    labs completed

---
+++ morning demo/fizzBuzz.js
@@ -0,0 +1,14 @@
+// Write your solution below this line:
+To do so, loop from 1 to 50 (inclusive). Each time through, if the number is evenly divisible by 3, say ‘fizz’. If the number is evenly divisible by 5, say ‘buzz’. If the number is evenly divisible by both 3 and 5, say ‘fizzbuzz’. Otherwise, say the number.
+//pseudocode:
+//iterate over the range 1 to 50, using a for-loop
+for (let i = 1; 1 < 51; i++) {
+
+//if true print out "fiz"
+//otherwise (else if), check if current number is divisible by 5
+if (i % 3 === 0)
+  // if true print out "fizz"
+  //otherwise, check if current number is divisible by both 3 and 5
+   //if trie print "fizzbuz"
+ } //else 
+  //print out current number 
