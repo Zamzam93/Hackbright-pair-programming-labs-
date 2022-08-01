17150de37deb930559f0e7e2097d6185238c4cd4
Zamzam93 <samira0101@live.com>
2022-08-01 15:38:02 -0500

    functionlab

---
+++ addToZero.js
@@ -0,0 +1,13 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, 12]
+
+// Write your solution below:
+let output = false;
+for (let i = 0; i < array.length; i++) {
+    for (let j = 0 + 1; j < array.length; j++) {
+     if (array[i] + array[i = 1]=== 0) {
+        output = true;
+     }
+    }
+}
+console.log(output);
